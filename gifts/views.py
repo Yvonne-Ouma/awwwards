c8ff0e22248b585407d91a134db157016f9230cd
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-10-18 09:04:14 -0400

    added model

--- gifts/views.py
+++ gifts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from .forms import SignupForm,ProjectForm,ProfileForm
+from .forms import SignupForm,ProjectForm,ProfileForm,ReviewForm
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
@@ -11,7 +11,7 @@ from .tokens import account_activation_token
 from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
 from django.contrib.auth.decorators import login_required
-from .models import Profile,Project
+from .models import Profile,Project,Review
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import ProjectSerializer
