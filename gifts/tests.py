1096d65ff450fa4b86cda3197534205836d3b718
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-10-18 12:17:16 -0400

    still texting

--- gifts/tests.py
+++ gifts/tests.py
@@ -51,4 +51,27 @@ class ProjectTestClass(TestCase):
         self.projectTest.delete()
 
 
+class ProjectTestClass(TestCase):
+    def setUp(self):
+        self.projectTest=User(user="user",name="blog",photo="gifts/media/images/blog.jpg",project_description="a better experience", project_url="https://moringaschool.instructure.com", technologies_used = "html")
+
+    def tearDown(self) :
+        User.objects.all().delete()
+
+    def test_instance(self):
+        self.assertIsInstance(self.projectTest,User)
+
+    def test_save_project(self):
+        self.assertFalse(self.projectTest in Project.objects.all())
+        self.projectTest.save()
+        self.assertTrue(self.projectTest in Project.objects.all())
+        self.projectTest.delete()
+
+    def test_delete_project(self):
+        self.assertTrue(self.projectTest in Project.objects.all())
+        self.projectTest.save()
+        self.assertFalse(self.projectTest in Project.objects.all())
+        self.projectTest.delete()
+
+
                      
