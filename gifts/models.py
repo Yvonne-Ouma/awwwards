4b012c5011c6360654e963c91e50b972b5c7bf87
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-10-18 09:35:01 -0400

    rated reviews for each project

--- gifts/models.py
+++ gifts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime as dt
-from django.core.validators import MinValueValidator,MaxValueValidator
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
